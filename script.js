05607549cc7b45ed8dd69871506bfc701d1b01e1
muazzam559 <145988892+muazzam559@users.noreply.github.com>
2023-10-18 10:52:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$('this').click(function(){
+    $(this).toggleclass('fa-times');
+    $('header').toggleclass('toggle');
+});
+$(window).on('scroll load',function(){
+    $('#menu').removeclass('fa-times');
+    $('header').removeclass('toggle');
+});
+});
